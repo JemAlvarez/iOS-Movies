3debf3eeaaa6758c770d9ce43e1d1520571c18c9
Jem Alvarez <jemalvarez22@gmail.com>
2020-07-02 13:27:46 -0400

    Movie view nav and movie backdrop

--- Movies/View/Screens/MovieView.swift
+++ Movies/View/Screens/MovieView.swift
@@ -9,17 +9,53 @@
 import SwiftUI
 
 struct MovieView: View {
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            ZStack (alignment: .top) {
+                VStack(alignment: .leading) {
+                    Image("joker_backdrop")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 350)
+                }
+                .offset(y: -45)
+                
+                MovieNavView()
+                    .frame(width: UIScreen.main.bounds.size.width)
+            }
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
+        }
+    }
+}
+
+struct MovieNavView: View {
     @Environment(\.presentationMode) var mode
     
     var body: some View {
-        VStack {
-            Button("BACK") {
+        HStack {
+            Button (action: {
                 self.mode.wrappedValue.dismiss()
+            }) {
+                HStack(spacing: 10) {
+                    Image(systemName: "chevron.left")
+                        .font(.system(size: 22))
+                    Text("Back")
+                        .font(.system(size: 22))
+                }
             }
-            .navigationBarBackButtonHidden(true)
             
             Spacer()
+            
+            Button (action: {
+                
+            }) {
+                Image(systemName: "square.and.arrow.up")
+                    .font(.system(size: 22))
+            }
         }
+        .foregroundColor(Color("main_gradient_1"))
+        .padding(.horizontal)
     }
 }
 
